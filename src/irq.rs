5aee4c91a57cc2ed69f2f7e3ea58f56c5a99996a
Gerd Zellweger <mail@gerdzellweger.com>
2015-03-29 23:13:15 +0200

    Added lidt and int! macro.

--- src/irq.rs
+++ src/irq.rs
@@ -9,3 +9,22 @@ pub unsafe fn disable()
 {
     asm!("cli");
 }
+
+/// Generate a software interrupt. 
+/// This is a macro argument needs to be an immediate.
+#[macro_export]
+macro_rules! int {
+    ( $x:expr ) => {
+        {
+            asm!("int $0" :: "N" ($x));
+        }
+    };
+}
+
+/// Load IDT table.
+pub unsafe fn lidt(idt: u64)
+{
+    asm!("lidt (%rax)" :: "{rax}" (idt));
+}
+
+
