35bcba2fa05911f7645d2836384585e1d8f62cba
Gerd Zellweger <mail@gerdzellweger.com>
2015-04-01 08:51:27 +0200

    IDT representations added.

--- src/irq.rs
+++ src/irq.rs
@@ -21,6 +21,36 @@ macro_rules! int {
     };
 }
 
+/// A struct describing an interrupt gate.
+#[derive(Debug, Copy)]
+#[repr(packed)]
+pub struct IdtEntry {
+    /// Lower 16 bits of ISR.
+    pub base_lo: u16,
+    /// Segment selector.
+    pub sel: u16,
+    /// This must always be zero.
+    pub res0: u8,
+    /// Flags.
+    pub flags: u8,
+    /// The upper 48 bits of ISR (the last 16 bits must be zero).
+    pub base_hi: u64,
+    /// Must be zero.
+    pub res1: u16
+}
+
+/// A struct describing a pointer to an array of interrupt handlers.
+/// This is in a format suitable for giving to 'lidt'.
+#[derive(Debug)]
+#[repr(packed)]
+pub struct IdtPointer
+{
+   /// Size of the IDT.
+   pub limit: u16,
+   /// Pointer to the memory region containing the IDT.
+   pub base: u64
+}
+
 /// Load IDT table.
 pub unsafe fn lidt(idt: u64)
 {
