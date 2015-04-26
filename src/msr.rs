967272550ac2ab3b5636a77b1da9a2011c00b72a
Gerd Zellweger <mail@gerdzellweger.com>
2015-04-26 11:35:03 +0200

    MSR registers.

--- src/msr.rs
+++ src/msr.rs
@@ -8,6 +8,8 @@ pub const IA32_STAR: u32 = 0xC0000081;
 /// IA-32e Mode System Call Target Address
 pub const IA32_LSTAR: u32 = 0xC0000082;
 
+/// System Call Flag Mask (R/W)
+pub const IA32_FMASK: u32 = 0xC0000084;
 
 /// Write 64 bits to msr register.
 pub unsafe fn wrmsr(msr: u32, value: u64) {
