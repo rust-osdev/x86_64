5bc5e1690a31c965e39a42fc90766caa7bb5a83d
Gerd Zellweger <mail@gerdzellweger.com>
2015-04-26 11:21:24 +0200

    Added IA32 register offsets.

--- src/msr.rs
+++ src/msr.rs
@@ -1,3 +1,14 @@
+
+/// Extended Feature Enables
+pub const IA32_EFER: u32 = 0xC0000080;
+
+/// System Call Target Address (R/W)
+pub const IA32_STAR: u32 = 0xC0000081;
+
+/// IA-32e Mode System Call Target Address
+pub const IA32_LSTAR: u32 = 0xC0000082;
+
+
 /// Write 64 bits to msr register.
 pub unsafe fn wrmsr(msr: u32, value: u64) {
     asm!("wrmsr" :: "{ecx}" (msr), "A" (value));
@@ -7,6 +18,6 @@ pub unsafe fn wrmsr(msr: u32, value: u64) {
 pub unsafe fn rdmsr(msr: u32) -> u64 {
     let mut val: u64;
     asm!("rdmsr" : "=A"(val) : "{ecx}" (msr));
-    
+
     return val;
 }
