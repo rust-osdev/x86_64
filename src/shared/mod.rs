f0d695c44622483fb1d1775fc9fd134cee3f9568
Rex Lunae <rexlunae@gmail.com>
2016-08-11 20:19:31 -0700

    Added nop as a function.

--- src/shared/mod.rs
+++ src/shared/mod.rs
@@ -23,3 +23,7 @@ pub enum PrivilegeLevel {
 pub unsafe fn halt() {
     asm!("hlt" :::: "volatile", "intel");
 }
+#[inline(always)]
+pub unsafe fn nop() {
+    asm!("nop" :::: "volatile", "intel");
+}
