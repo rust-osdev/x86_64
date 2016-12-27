e7ffc39fd87cf704cbbc51eebba7fd9bd7058628
Philipp Oppermann <dev@phil-opp.com>
2016-12-27 19:03:34 +0100

    Fork as “x86_64”

--- tests/no_std_build.rs
+++ tests/no_std_build.rs
@@ -2,12 +2,16 @@
 #![no_std]
 
 extern crate libc;
-extern crate x86;
+extern crate x86_64;
 
 #[start]
 fn start(_argc: isize, _argv: *const *const u8) -> isize {
     0
 }
 
-#[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] fn panic_fmt() -> ! { loop {} }
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
+#[lang = "panic_fmt"]
+fn panic_fmt() -> ! {
+    loop {}
+}
