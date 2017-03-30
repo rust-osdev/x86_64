0e6cd75683c5103500bf14151f12da04d7b23d2d
Philipp Oppermann <dev@phil-opp.com>
2017-03-30 12:20:09 +0200

    Improve Debug formatting

--- src/address.rs
+++ src/address.rs
@@ -1,8 +1,14 @@
 use core::fmt;
 
 /// Represents a physical memory address
-#[derive(Copy, Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+#[derive(Copy, Clone, Eq, Ord, PartialEq, PartialOrd)]
 pub struct PhysicalAddress(pub u64);
+
+impl fmt::Debug for PhysicalAddress {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:#x}", self.0)
+    }
+}
 
 impl fmt::Binary for PhysicalAddress {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -35,8 +41,14 @@ impl fmt::UpperHex for PhysicalAddress {
 }
 
 /// Represent a virtual (linear) memory address
-#[derive(Copy, Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+#[derive(Copy, Clone, Eq, Ord, PartialEq, PartialOrd)]
 pub struct VirtualAddress(pub usize);
+
+impl fmt::Debug for VirtualAddress {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:#x}", self.0)
+    }
+}
 
 impl fmt::Binary for VirtualAddress {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
--- src/structures/idt.rs
+++ src/structures/idt.rs
@@ -9,11 +9,12 @@
 
 //! Provides types for the Interrupt Descriptor Table and its entries.
 
+use core::fmt;
 use core::marker::PhantomData;
 use core::mem;
 use core::ops::{Index, IndexMut};
 use bit_field::BitField;
-use PrivilegeLevel;
+use {PrivilegeLevel, VirtualAddress};
 
 /// An Interrupt Descriptor Table with 256 entries.
 ///
@@ -588,7 +589,6 @@ impl EntryOptions {
 }
 
 /// Represents the exception stack frame pushed by the CPU on exception entry.
-#[derive(Debug)]
 #[repr(C)]
 pub struct ExceptionStackFrame {
     /// This value points to the instruction that should be executed when the interrupt
@@ -596,15 +596,34 @@ pub struct ExceptionStackFrame {
     /// following the last executed instruction. However, for some exceptions (e.g., page faults),
     /// this value points to the faulting instruction, so that the instruction is restarted on
     /// return. See the documentation of the `Idt` fields for more details.
-    pub instruction_pointer: u64,
+    pub instruction_pointer: VirtualAddress,
     /// The code segment selector, padded with zeros.
     pub code_segment: u64,
     /// The flags register before the interrupt handler was invoked.
     pub cpu_flags: u64,
     /// The stack pointer at the time of the interrupt.
-    pub stack_pointer: u64,
+    pub stack_pointer: VirtualAddress,
     /// The stack segment descriptor at the time of the interrupt (often zero in 64-bit mode).
     pub stack_segment: u64,
+}
+
+impl fmt::Debug for ExceptionStackFrame {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        struct Hex(u64);
+        impl fmt::Debug for Hex {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "{:#x}", self.0)
+            }
+        }
+
+        let mut s = f.debug_struct("ExceptionStackFrame");
+        s.field("instruction_pointer", &self.instruction_pointer);
+        s.field("code_segment", &self.code_segment);
+        s.field("cpu_flags", &Hex(self.cpu_flags));
+        s.field("stack_pointer", &self.stack_pointer);
+        s.field("stack_segment", &self.stack_segment);
+        s.finish()
+    }
 }
 
 bitflags! {
