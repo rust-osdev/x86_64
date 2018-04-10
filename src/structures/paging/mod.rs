399b3d4d4743559317c9e0a886caabf5b68e02c7
Philipp Oppermann <dev@phil-opp.com>
2018-04-10 23:25:11 +0200

    Make Page and Frame generic over page size

--- src/structures/paging/mod.rs
+++ src/structures/paging/mod.rs
@@ -3,44 +3,80 @@
 pub use self::page_table::*;
 pub use self::recursive::*;
 
-use addr::{PhysAddr, VirtAddr};
+use {VirtAddr, PhysAddr};
 use core::ops::{Add, AddAssign, Sub, SubAssign};
+use core::marker::PhantomData;
+use core::fmt;
 use ux::*;
 
 mod page_table;
 mod recursive;
 
-/// The default page size on x86_64.
-pub const PAGE_SIZE: u16 = 4096;
-
-/// A virtual 4kB page.
-#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
-pub struct Page {
-    number: u64,
+pub trait PageSize: Copy + Eq + PartialOrd + Ord {
+    const SIZE: u64;
+    const SIZE_AS_DEBUG_STR: &'static str;
 }
 
-impl Page {
-    /// Returns the page that contains the given virtual address.
-    pub fn containing_address(address: VirtAddr) -> Page {
-        Page {
-            number: address.as_u64() / u64::from(PAGE_SIZE),
+pub trait NotGiantPageSize: PageSize {}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Size4KB {}
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Size2MB {}
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Size1GB {}
+
+impl PageSize for Size4KB {
+    const SIZE: u64 = 4096;
+    const SIZE_AS_DEBUG_STR: &'static str = "4KB";
+}
+impl NotGiantPageSize for Size4KB {}
+
+impl PageSize for Size2MB {
+    const SIZE: u64 = Size4KB::SIZE * 512;
+    const SIZE_AS_DEBUG_STR: &'static str = "2MB";
+}
+impl NotGiantPageSize for Size2MB {}
+
+impl PageSize for Size1GB {
+    const SIZE: u64 = Size2MB::SIZE * 512;
+    const SIZE_AS_DEBUG_STR: &'static str = "1GB";
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[repr(C)]
+pub struct Page<S: PageSize = Size4KB> {
+    start_address: VirtAddr,
+    size: PhantomData<S>,
+}
+
+impl<S: PageSize> Page<S> {
+    /// Returns the page that starts at the given virtual address.
+    ///
+    /// Returns an error if the address is not correctly aligned (i.e. is not a valid page start).
+    pub fn from_start_address(address: VirtAddr) -> Result<Self, ()> {
+        if !address.is_aligned(S::SIZE) {
+            return Err(());
         }
+        Ok(Page::containing_address(address))
     }
 
-    pub fn from_page_table_indices(p4_index: u9, p3_index: u9, p2_index: u9, p1_index: u9) -> Page {
-        use bit_field::BitField;
-
-        let mut addr = 0;
-        addr.set_bits(39..48, u64::from(p4_index));
-        addr.set_bits(30..39, u64::from(p3_index));
-        addr.set_bits(21..30, u64::from(p2_index));
-        addr.set_bits(12..21, u64::from(p1_index));
-        Page::containing_address(VirtAddr::new(addr))
+    /// Returns the page that contains the given virtual address.
+    pub fn containing_address(address: VirtAddr) -> Self {
+        Page {
+            start_address: address.align_down(S::SIZE),
+            size: PhantomData,
+        }
     }
 
     /// Returns the start address of the page.
     pub fn start_address(&self) -> VirtAddr {
-        VirtAddr::new(self.number * u64::from(PAGE_SIZE))
+        self.start_address
+    }
+
+    /// Returns the size the page (4KB, 2MB or 1GB).
+    pub const fn size(&self) -> u64 {
+        S::SIZE
     }
 
     /// Returns the level 4 page table index of this page.
@@ -53,67 +89,120 @@ impl Page {
         self.start_address().p3_index()
     }
 
+    pub fn range(start: Self, end: Self) -> PageRange<S> {
+        PageRange { start, end }
+    }
+
+    pub fn range_inclusive(start: Self, end: Self) -> PageRangeInclusive<S> {
+        PageRangeInclusive { start, end }
+    }
+}
+
+impl<S: NotGiantPageSize> Page<S> {
     /// Returns the level 2 page table index of this page.
     pub fn p2_index(&self) -> u9 {
         self.start_address().p2_index()
+    }
+}
+
+impl Page<Size1GB> {
+    pub fn from_page_table_indices_1gb(p4_index: u9, p3_index: u9) -> Self {
+        use bit_field::BitField;
+
+        let mut addr = 0;
+        addr.set_bits(39..48, u64::from(p4_index));
+        addr.set_bits(30..39, u64::from(p3_index));
+        Page::containing_address(VirtAddr::new(addr))
+    }
+}
+
+impl Page<Size2MB> {
+    pub fn from_page_table_indices_2mb(p4_index: u9, p3_index: u9, p2_index: u9) -> Self {
+        use bit_field::BitField;
+
+        let mut addr = 0;
+        addr.set_bits(39..48, u64::from(p4_index));
+        addr.set_bits(30..39, u64::from(p3_index));
+        addr.set_bits(21..30, u64::from(p2_index));
+        Page::containing_address(VirtAddr::new(addr))
+    }
+}
+
+impl Page<Size4KB> {
+    pub fn from_page_table_indices(p4_index: u9, p3_index: u9, p2_index: u9, p1_index: u9) -> Self {
+        use bit_field::BitField;
+
+        let mut addr = 0;
+        addr.set_bits(39..48, u64::from(p4_index));
+        addr.set_bits(30..39, u64::from(p3_index));
+        addr.set_bits(21..30, u64::from(p2_index));
+        addr.set_bits(12..21, u64::from(p1_index));
+        Page::containing_address(VirtAddr::new(addr))
     }
 
     /// Returns the level 1 page table index of this page.
     pub fn p1_index(&self) -> u9 {
         self.start_address().p1_index()
     }
+}
 
-    pub fn range(start: Page, end: Page) -> PageRange {
-        PageRange { start, end }
-    }
-
-    pub fn range_inclusive(start: Page, end: Page) -> PageRangeInclusive {
-        PageRangeInclusive { start, end }
+impl<S: PageSize> fmt::Debug for Page<S> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_fmt(format_args!("Page[{}]({:#x})", S::SIZE_AS_DEBUG_STR,
+            self.start_address().as_u64()))
     }
 }
 
-impl Add<u64> for Page {
+impl<S: PageSize> Add<u64> for Page<S> {
     type Output = Self;
     fn add(self, rhs: u64) -> Self::Output {
-        Page::containing_address(self.start_address() + rhs * u64::from(PAGE_SIZE))
+        Page::containing_address(self.start_address() + rhs * u64::from(S::SIZE))
     }
 }
 
-impl AddAssign<u64> for Page {
+impl<S: PageSize> AddAssign<u64> for Page<S> {
     fn add_assign(&mut self, rhs: u64) {
         *self = self.clone() + rhs;
     }
 }
 
-impl Sub<u64> for Page {
+impl<S: PageSize> Sub<u64> for Page<S> {
     type Output = Self;
     fn sub(self, rhs: u64) -> Self::Output {
-        Page::containing_address(self.start_address() - rhs * u64::from(PAGE_SIZE))
+        Page::containing_address(self.start_address() - rhs * u64::from(S::SIZE))
     }
 }
 
-impl SubAssign<u64> for Page {
+impl<S: PageSize> SubAssign<u64> for Page<S> {
     fn sub_assign(&mut self, rhs: u64) {
         *self = self.clone() - rhs;
     }
 }
 
-impl Sub<Page> for Page {
+impl<S: PageSize> Sub<Self> for Page<S> {
     type Output = u64;
-    fn sub(self, rhs: Page) -> Self::Output {
-        self.number.checked_sub(rhs.number).unwrap()
+    fn sub(self, rhs: Self) -> Self::Output {
+        (self.start_address - rhs.start_address) / S::SIZE
     }
 }
 
-pub struct PageRange {
-    pub start: Page,
-    pub end: Page,
+#[derive(Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+pub struct PageRange<S: PageSize = Size4KB> {
+    pub start: Page<S>,
+    pub end: Page<S>,
 }
 
-impl Iterator for PageRange {
-    type Item = Page;
+impl<S: PageSize> PageRange<S> {
+    pub fn is_empty(&self) -> bool {
+        !(self.start < self.end)
+    }
+}
 
-    fn next(&mut self) -> Option<Page> {
+impl<S: PageSize> Iterator for PageRange<S> {
+    type Item = Page<S>;
+
+    fn next(&mut self) -> Option<Self::Item> {
         if self.start < self.end {
             let page = self.start.clone();
             self.start += 1;
@@ -124,15 +213,41 @@ impl Iterator for PageRange {
     }
 }
 
-pub struct PageRangeInclusive {
-    pub start: Page,
-    pub end: Page,
+impl PageRange<Size2MB> {
+    pub fn as_4kb_page_range(self) -> PageRange<Size4KB> {
+        PageRange {
+            start: Page::containing_address(self.start.start_address()),
+            end: Page::containing_address(self.end.start_address()),
+        }
+    }
 }
 
-impl Iterator for PageRangeInclusive {
-    type Item = Page;
+impl<S: PageSize> fmt::Debug for PageRange<S> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("PageRange")
+            .field("start", &self.start)
+            .field("end", &self.end)
+            .finish()
+    }
+}
 
-    fn next(&mut self) -> Option<Page> {
+#[derive(Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+pub struct PageRangeInclusive<S: PageSize = Size4KB> {
+    pub start: Page<S>,
+    pub end: Page<S>,
+}
+
+impl<S: PageSize> PageRangeInclusive<S> {
+    pub fn is_empty(&self) -> bool {
+        !(self.start <= self.end)
+    }
+}
+
+impl<S: PageSize> Iterator for PageRangeInclusive<S> {
+    type Item = Page<S>;
+
+    fn next(&mut self) -> Option<Self::Item> {
         if self.start <= self.end {
             let page = self.start.clone();
             self.start += 1;
@@ -143,76 +258,117 @@ impl Iterator for PageRangeInclusive {
     }
 }
 
-/// A physical 4kB frame.
-#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
-pub struct PhysFrame {
-    number: u64,
+impl<S: PageSize> fmt::Debug for PageRangeInclusive<S> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("PageRangeInclusive")
+            .field("start", &self.start)
+            .field("end", &self.end)
+            .finish()
+    }
 }
 
-impl PhysFrame {
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[repr(C)]
+pub struct PhysFrame<S: PageSize = Size4KB> {
+    start_address: PhysAddr,
+    size: PhantomData<S>,
+}
+
+impl<S: PageSize> PhysFrame<S> {
+    /// Returns the frame that starts at the given virtual address.
+    ///
+    /// Returns an error if the address is not correctly aligned (i.e. is not a valid frame start).
+    pub fn from_start_address(address: PhysAddr) -> Result<Self, ()> {
+        if !address.is_aligned(S::SIZE) {
+            return Err(());
+        }
+        Ok(PhysFrame::containing_address(address))
+    }
+
     /// Returns the frame that contains the given physical address.
-    pub fn containing_address(address: PhysAddr) -> PhysFrame {
+    pub fn containing_address(address: PhysAddr) -> Self {
         PhysFrame {
-            number: address.as_u64() / u64::from(PAGE_SIZE),
+            start_address: address.align_down(S::SIZE),
+            size: PhantomData,
         }
     }
 
     /// Returns the start address of the page.
     pub fn start_address(&self) -> PhysAddr {
-        PhysAddr::new(self.number * u64::from(PAGE_SIZE))
+        self.start_address
     }
 
-    pub fn range(start: PhysFrame, end: PhysFrame) -> PhysFrameRange {
+    /// Returns the size the frame (4KB, 2MB or 1GB).
+    pub fn size(&self) -> u64 {
+        S::SIZE
+    }
+
+    pub fn range(start: PhysFrame<S>, end: PhysFrame<S>) -> PhysFrameRange<S> {
         PhysFrameRange { start, end }
     }
 
-    pub fn range_inclusive(start: PhysFrame, end: PhysFrame) -> PhysFrameRangeInclusive {
+    pub fn range_inclusive(start: PhysFrame<S>, end: PhysFrame<S>) -> PhysFrameRangeInclusive<S> {
         PhysFrameRangeInclusive { start, end }
     }
 }
 
-impl Add<u64> for PhysFrame {
-    type Output = Self;
-    fn add(self, rhs: u64) -> Self::Output {
-        PhysFrame::containing_address(self.start_address() + rhs * u64::from(PAGE_SIZE))
+impl<S: PageSize> fmt::Debug for PhysFrame<S> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_fmt(format_args!("PhysFrame[{}]({:#x})", S::SIZE_AS_DEBUG_STR,
+            self.start_address().as_u64()))
     }
 }
 
-impl AddAssign<u64> for PhysFrame {
+impl<S: PageSize> Add<u64> for PhysFrame<S> {
+    type Output = Self;
+    fn add(self, rhs: u64) -> Self::Output {
+        PhysFrame::containing_address(self.start_address() + rhs * u64::from(S::SIZE))
+    }
+}
+
+impl<S: PageSize> AddAssign<u64> for PhysFrame<S> {
     fn add_assign(&mut self, rhs: u64) {
         *self = self.clone() + rhs;
     }
 }
 
-impl Sub<u64> for PhysFrame {
+impl<S: PageSize> Sub<u64> for PhysFrame<S> {
     type Output = Self;
     fn sub(self, rhs: u64) -> Self::Output {
-        PhysFrame::containing_address(self.start_address() - rhs * u64::from(PAGE_SIZE))
+        PhysFrame::containing_address(self.start_address() - rhs * u64::from(S::SIZE))
     }
 }
 
-impl SubAssign<u64> for PhysFrame {
+impl<S: PageSize> SubAssign<u64> for PhysFrame<S> {
     fn sub_assign(&mut self, rhs: u64) {
         *self = self.clone() - rhs;
     }
 }
 
-impl Sub<PhysFrame> for PhysFrame {
+impl<S: PageSize> Sub<PhysFrame<S>> for PhysFrame<S> {
     type Output = u64;
-    fn sub(self, rhs: PhysFrame) -> Self::Output {
-        self.number.checked_sub(rhs.number).unwrap()
+    fn sub(self, rhs: PhysFrame<S>) -> Self::Output {
+        (self.start_address - rhs.start_address) / S::SIZE
     }
 }
 
-pub struct PhysFrameRange {
-    pub start: PhysFrame,
-    pub end: PhysFrame,
+#[derive(Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+pub struct PhysFrameRange<S: PageSize = Size4KB> {
+    pub start: PhysFrame<S>,
+    pub end: PhysFrame<S>,
 }
 
-impl Iterator for PhysFrameRange {
-    type Item = PhysFrame;
+impl<S: PageSize> PhysFrameRange<S> {
+    pub fn is_empty(&self) -> bool {
+        !(self.start < self.end)
+    }
+}
 
-    fn next(&mut self) -> Option<PhysFrame> {
+impl<S: PageSize> Iterator for PhysFrameRange<S> {
+    type Item = PhysFrame<S>;
+
+    fn next(&mut self) -> Option<Self::Item> {
         if self.start < self.end {
             let frame = self.start.clone();
             self.start += 1;
@@ -223,15 +379,32 @@ impl Iterator for PhysFrameRange {
     }
 }
 
-pub struct PhysFrameRangeInclusive {
-    pub start: PhysFrame,
-    pub end: PhysFrame,
+impl<S: PageSize> fmt::Debug for PhysFrameRange<S> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("PhysFrameRange")
+            .field("start", &self.start)
+            .field("end", &self.end)
+            .finish()
+    }
 }
 
-impl Iterator for PhysFrameRangeInclusive {
-    type Item = PhysFrame;
+#[derive(Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+pub struct PhysFrameRangeInclusive<S: PageSize = Size4KB> {
+    pub start: PhysFrame<S>,
+    pub end: PhysFrame<S>,
+}
 
-    fn next(&mut self) -> Option<PhysFrame> {
+impl<S: PageSize> PhysFrameRangeInclusive<S> {
+    pub fn is_empty(&self) -> bool {
+        !(self.start <= self.end)
+    }
+}
+
+impl<S: PageSize> Iterator for PhysFrameRangeInclusive<S> {
+    type Item = PhysFrame<S>;
+
+    fn next(&mut self) -> Option<Self::Item> {
         if self.start <= self.end {
             let frame = self.start.clone();
             self.start += 1;
@@ -239,6 +412,15 @@ impl Iterator for PhysFrameRangeInclusive {
         } else {
             None
         }
+    }
+}
+
+impl<S: PageSize> fmt::Debug for PhysFrameRangeInclusive<S> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("PhysFrameRangeInclusive")
+            .field("start", &self.start)
+            .field("end", &self.end)
+            .finish()
     }
 }
 
