3a2d833bf4df18307ec8873ca5461ae12228cd26
Mark Mansi <markm@cs.wisc.edu>
2018-05-07 16:02:56 -0500

    idt EntryOptions must be repr C

--- src/structures/idt.rs
+++ src/structures/idt.rs
@@ -537,6 +537,7 @@ impl_set_handler_fn!(HandlerFuncWithErrCode);
 impl_set_handler_fn!(PageFaultHandlerFunc);
 
 /// Represents the options field of an IDT entry.
+#[repr(C)]
 #[derive(Debug, Clone, Copy)]
 pub struct EntryOptions(u16);
 
