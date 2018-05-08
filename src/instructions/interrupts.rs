5d09478bb10ea1e7873fca6ac36952be1b4802e0
Mark Mansi <markm@cs.wisc.edu>
2018-05-07 21:47:52 -0500

    add interrupts functions

---
+++ src/instructions/interrupts.rs
@@ -0,0 +1,28 @@
+//! Enabling and disabling interrupts
+
+/// Enable interrupts. This is a wrapper around `sti`.
+pub fn enable() {
+    asm!("sti")
+}
+
+/// Disable interrupts. This is a wrapper around `cli`.
+pub fn disable() {
+    asm!("cli")
+}
+
+/// Run the given closure, disabling interrupts before running it and enabling interrupts
+/// afterwards.
+///
+/// # Note
+///
+/// This function basically just does `disable`, runs the closure, the does `enable`. If you have
+/// other `enable` and `disable` calls _within_ the closure, things may not work as expected.
+pub fn without_interrupts<F, R>(f: F) -> R
+where
+    F: FnOnce() -> R,
+{
+    disable();
+    let ret = f();
+    enable();
+    ret
+}
