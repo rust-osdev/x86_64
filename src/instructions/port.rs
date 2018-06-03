97c4fe0b144309606193b58198bed57bd3f06fd3
Philipp Oppermann <dev@phil-opp.com>
2018-06-03 13:05:29 +0200

    Rename PortReadWrite::{read, write} functions

--- src/instructions/port.rs
+++ src/instructions/port.rs
@@ -3,48 +3,48 @@
 use core::marker::PhantomData;
 
 pub trait PortReadWrite {
-    unsafe fn read(port: u16) -> Self;
-    unsafe fn write(port: u16, value: Self);
+    unsafe fn read_from_port(port: u16) -> Self;
+    unsafe fn write_to_port(port: u16, value: Self);
 }
 
 impl PortReadWrite for u8 {
     #[inline]
-    unsafe fn read(port: u16) -> u8 {
+    unsafe fn read_from_port(port: u16) -> u8 {
         let value: u8;
         asm!("inb %dx, %al" : "={ax}"(value) : "{dx}"(port) :: "volatile");
         value
     }
 
     #[inline]
-    unsafe fn write(port: u16, value: u8) {
+    unsafe fn write_to_port(port: u16, value: u8) {
         asm!("outb %al, %dx" :: "{dx}"(port), "{al}"(value) :: "volatile");
     }
 }
 
 impl PortReadWrite for u16 {
     #[inline]
-    unsafe fn read(port: u16) -> u16 {
+    unsafe fn read_from_port(port: u16) -> u16 {
         let value: u16;
         asm!("inw %dx, %ax" : "={ax}"(value) : "{dx}"(port) :: "volatile");
         value
     }
 
     #[inline]
-    unsafe fn write(port: u16, value: u16) {
+    unsafe fn write_to_port(port: u16, value: u16) {
         asm!("outw %ax, %dx" :: "{dx}"(port), "{al}"(value) :: "volatile");
     }
 }
 
 impl PortReadWrite for u32 {
     #[inline]
-    unsafe fn read(port: u16) -> u32 {
+    unsafe fn read_from_port(port: u16) -> u32 {
         let value: u32;
         asm!("inl %dx, %eax" : "={ax}"(value) : "{dx}"(port) :: "volatile");
         value
     }
 
     #[inline]
-    unsafe fn write(port: u16, value: u32) {
+    unsafe fn write_to_port(port: u16, value: u32) {
         asm!("outl %eax, %dx" :: "{dx}"(port), "{al}"(value) :: "volatile");
     }
 }
@@ -64,11 +64,11 @@ impl<T: PortReadWrite> Port<T> {
 
     #[inline]
     pub unsafe fn read(&self) -> T {
-        T::read(self.port)
+        T::read_from_port(self.port)
     }
 
     #[inline]
     pub unsafe fn write(&mut self, value: T) {
-        T::write(self.port, value)
+        T::write_to_port(self.port, value)
     }
 }
