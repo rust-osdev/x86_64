7ff1b884490ad40c3132ce4e17a62b56d0746d0a
Philipp Oppermann <dev@phil-opp.com>
2018-06-17 18:13:38 +0200

    Add int3 instruction

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -5,3 +5,10 @@ pub mod port;
 pub mod segmentation;
 pub mod tables;
 pub mod tlb;
+
+/// Cause a breakpoint exception by invoking the `int3` instruction.
+pub fn int3() {
+    unsafe {
+        asm!("int3");
+    }
+}
