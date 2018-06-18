1de06f3809b44b23db4f37298050b355e0f6dfb9
Philipp Oppermann <dev@phil-opp.com>
2018-06-18 12:28:07 +0200

    Add basic types for GDT and GDT descriptors

--- src/structures/gdt.rs
+++ src/structures/gdt.rs
@@ -2,6 +2,7 @@
 
 use bit_field::BitField;
 use core::fmt;
+use structures::tss::TaskStateSegment;
 use PrivilegeLevel;
 
 /// Specifies which element to load into a segment from
@@ -38,5 +39,100 @@ impl fmt::Debug for SegmentSelector {
         s.field("index", &self.index());
         s.field("rpl", &self.rpl());
         s.finish()
+    }
+}
+
+pub struct GlobalDescriptorTable {
+    table: [u64; 8],
+    next_free: usize,
+}
+
+impl GlobalDescriptorTable {
+    pub fn new() -> GlobalDescriptorTable {
+        GlobalDescriptorTable {
+            table: [0; 8],
+            next_free: 1,
+        }
+    }
+
+    pub fn add_entry(&mut self, entry: Descriptor) -> SegmentSelector {
+        let index = match entry {
+            Descriptor::UserSegment(value) => self.push(value),
+            Descriptor::SystemSegment(value_low, value_high) => {
+                let index = self.push(value_low);
+                self.push(value_high);
+                index
+            }
+        };
+        SegmentSelector::new(index as u16, PrivilegeLevel::Ring0)
+    }
+
+    pub fn load(&'static self) {
+        use core::mem::size_of;
+        use instructions::tables::{lgdt, DescriptorTablePointer};
+
+        let ptr = DescriptorTablePointer {
+            base: self.table.as_ptr() as u64,
+            limit: (self.table.len() * size_of::<u64>() - 1) as u16,
+        };
+
+        unsafe { lgdt(&ptr) };
+    }
+
+    fn push(&mut self, value: u64) -> usize {
+        if self.next_free < self.table.len() {
+            let index = self.next_free;
+            self.table[index] = value;
+            self.next_free += 1;
+            index
+        } else {
+            panic!("GDT full");
+        }
+    }
+}
+
+pub enum Descriptor {
+    UserSegment(u64),
+    SystemSegment(u64, u64),
+}
+
+bitflags! {
+    pub struct DescriptorFlags: u64 {
+        const CONFORMING        = 1 << 42;
+        const EXECUTABLE        = 1 << 43;
+        const USER_SEGMENT      = 1 << 44;
+        const PRESENT           = 1 << 47;
+        const LONG_MODE         = 1 << 53;
+    }
+}
+
+impl Descriptor {
+    pub fn kernel_code_segment() -> Descriptor {
+        use self::DescriptorFlags as Flags;
+
+        let flags = Flags::USER_SEGMENT | Flags::PRESENT | Flags::EXECUTABLE | Flags::LONG_MODE;
+        Descriptor::UserSegment(flags.bits())
+    }
+
+    pub fn tss_segment(tss: &'static TaskStateSegment) -> Descriptor {
+        use self::DescriptorFlags as Flags;
+        use bit_field::BitField;
+        use core::mem::size_of;
+
+        let ptr = tss as *const _ as u64;
+
+        let mut low = Flags::PRESENT.bits();
+        // base
+        low.set_bits(16..40, ptr.get_bits(0..24));
+        low.set_bits(56..64, ptr.get_bits(24..32));
+        // limit (the `-1` in needed since the bound is inclusive)
+        low.set_bits(0..16, (size_of::<TaskStateSegment>() - 1) as u64);
+        // type (0b1001 = available 64-bit tss)
+        low.set_bits(40..44, 0b1001);
+
+        let mut high = 0;
+        high.set_bits(0..32, ptr.get_bits(32..64));
+
+        Descriptor::SystemSegment(low, high)
     }
 }
