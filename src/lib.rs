1ba3e7e10a1658fdbb23350bf9034e31a4d38952
Kamil Koczurek <koczurekk@gmail.com>
2019-02-03 13:45:30 +0100

    Support for RdRand (#57)
    * Added a dependency on raw-cpuid
    
    * Implemented pub fn rdrand() -> Option<u64>
    
    * Switched from spaces to tabs to match current x86_64 style
    
    * Depend on raw_cpuid only when building for x86_64
    
    * Replaced rdrand() -> Option<u64> with struct RdRand
    
    * Moved RdRand to src/random/mod.rs and added module random to lib.rs
    
    * Updated docs

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,7 @@ pub use ux;
 pub use crate::addr::{align_down, align_up, PhysAddr, VirtAddr};
 
 pub mod instructions;
+pub mod random;
 pub mod registers;
 pub mod structures;
 
---
+++ src/random/mod.rs
@@ -0,0 +1,34 @@
+//! Support for build-in RNGs
+
+#[derive(Debug)]
+/// Used to obtain random numbers using x86_64's RDRAND opcode
+pub struct RdRand(());
+
+impl RdRand {
+    /// Creates Some(RdRand) if RDRAND is supported, None otherwise
+    #[cfg(target_arch = "x86_64")]
+    pub fn new() -> Option<Self> {
+        let cpuid = raw_cpuid::CpuId::new();
+        let has_rdrand = match cpuid.get_feature_info() {
+            Some(finfo) => finfo.has_rdrand(),
+            None => false
+        };
+
+        match has_rdrand {
+            true => Some(RdRand(())),
+            false => None
+        }
+    }
+
+    /// Uniformly sampled u64
+    #[inline]
+    #[cfg(target_arch = "x86_64")]
+    pub fn get(&self) -> u64 {
+        let res: u64;
+        unsafe {
+            asm!("rdrand %rax" : "={rax}"(res) ::: "volatile");
+        }
+
+        res
+    }
+}
