18743cf8ff596aed4045093c7308d5d79cbacfd4
Tyler Ruckinger <t.ruckinger@gmail.com>
2019-02-07 18:41:00 -0500

    Handle failure of rdrand instruction
    Make RdRand struct Copy, Clone
    
    Add RdRandPrimitive trait
    
    Generalize RdRand::get() to return any RdRandPrimitive type
    
    Add helper fns to get u64, u32, or u16 explicitly

--- src/random/mod.rs
+++ src/random/mod.rs
@@ -1,12 +1,19 @@
 //! Support for build-in RNGs
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 /// Used to obtain random numbers using x86_64's RDRAND opcode
 pub struct RdRand(());
 
+/// Trait for types that can be returned by the RDRAND instruction
+pub trait RdRandPrimitive: Sized {}
+impl RdRandPrimitive for u16 {}
+impl RdRandPrimitive for u32 {}
+#[cfg(target_arch = "x86_64")]
+impl RdRandPrimitive for u64 {}
+
 impl RdRand {
     /// Creates Some(RdRand) if RDRAND is supported, None otherwise
-    #[cfg(target_arch = "x86_64")]
+    #[cfg(target_arch = "x86_64")] // <-- raw_cpuid is only a dep for x86_64 targets
     pub fn new() -> Option<Self> {
         let cpuid = raw_cpuid::CpuId::new();
         let has_rdrand = match cpuid.get_feature_info() {
@@ -20,15 +27,35 @@ impl RdRand {
         }
     }
 
-    /// Uniformly sampled u64
+    /// Uniformly sampled u64, u32, or u16
     #[inline]
-    #[cfg(target_arch = "x86_64")]
-    pub fn get(&self) -> u64 {
-        let res: u64;
+    pub fn get<T: RdRandPrimitive>(&self) -> Option<T> {
+        let res: T;
+        let ok: u8;
         unsafe {
-            asm!("rdrand %rax" : "={rax}"(res) ::: "volatile");
+            asm!("rdrand $0; setc $1;"
+                : "=r"(res) "=r"(ok)
+                :: "flags" : "volatile");
         }
-
-        res
+        match ok {
+            1 => Some(res),
+            _ => None
+        }
+    }
+    /// Uniformly sampled u64
+    #[cfg(target_arch = "x86_64")]
+    #[inline]
+    pub fn get_u64(&self) -> Option<u64> {
+        self.get()
+    }
+    /// Uniformly sampled u32
+    #[inline]
+    pub fn get_u32(&self) -> Option<u32> {
+        self.get()
+    }
+    /// Uniformly sampled u16
+    #[inline]
+    pub fn get_u16(&self) -> Option<u16> {
+        self.get()
     }
 }
