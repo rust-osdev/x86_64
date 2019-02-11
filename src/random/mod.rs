25ab6f54f7306f3473c242ce4c5428fda51aaa87
Tyler Ruckinger <t.ruckinger@gmail.com>
2019-02-11 04:37:31 -0500

    Handle failure of rdrand instruction (#59)
    - Make RdRand struct Copy, Clone
    - Add helper fns to get u64, u32, or u16 explicitly
    - Add note to docs that the instruction can fail
    - Deprecate original get function

--- src/random/mod.rs
+++ src/random/mod.rs
@@ -1,12 +1,12 @@
 //! Support for build-in RNGs
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 /// Used to obtain random numbers using x86_64's RDRAND opcode
 pub struct RdRand(());
 
+#[cfg(target_arch = "x86_64")]
 impl RdRand {
     /// Creates Some(RdRand) if RDRAND is supported, None otherwise
-    #[cfg(target_arch = "x86_64")]
     pub fn new() -> Option<Self> {
         let cpuid = raw_cpuid::CpuId::new();
         let has_rdrand = match cpuid.get_feature_info() {
@@ -21,8 +21,8 @@ impl RdRand {
     }
 
     /// Uniformly sampled u64
+    #[deprecated(note = "This method does not check for errors and will be removed in the next breaking release. Use get_u64() instead.")]
     #[inline]
-    #[cfg(target_arch = "x86_64")]
     pub fn get(&self) -> u64 {
         let res: u64;
         unsafe {
@@ -30,5 +30,53 @@ impl RdRand {
         }
 
         res
+    }
+    /// Uniformly sampled u64.
+    /// May fail in rare circumstances or heavy load.
+    #[inline]
+    pub fn get_u64(&self) -> Option<u64> {
+        let res: u64;
+        let ok: u8;
+        unsafe {
+            asm!("rdrand %rax; setc $1;"
+                : "={rax}"(res) "=r"(ok)
+                :: "flags" : "volatile");
+        }
+        match ok {
+            1 => Some(res),
+            _ => None
+        }
+    }
+    /// Uniformly sampled u32.
+    /// May fail in rare circumstances or heavy load.
+    #[inline]
+    pub fn get_u32(&self) -> Option<u32> {
+        let res: u32;
+        let ok: u8;
+        unsafe {
+            asm!("rdrand %eax; setc $1;"
+                : "={eax}"(res) "=r"(ok)
+                :: "flags" : "volatile");
+        }
+        match ok {
+            1 => Some(res),
+            _ => None
+        }
+    }
+    /// Uniformly sampled u16.
+    /// May fail in rare circumstances or heavy load.
+    #[inline]
+    pub fn get_u16(&self) -> Option<u16> {
+        let res: u16;
+        let ok: u8;
+        unsafe {
+            asm!("rdrand %ax; setc $1;"
+                : "={ax}"(res) "=r"(ok)
+                :: "flags" : "volatile");
+        }
+        match ok {
+            1 => Some(res),
+            _ => None
+        }
     }
 }
