e1d844b603b858bfaad35e0cdc2cd2367e897924
Ryan Kennedy <rkennedy9064@gmail.com>
2019-03-19 20:42:46 -0500

    Implement ReadOnlyPort, WriteOnlyPort and Port

--- src/structures/port.rs
+++ src/structures/port.rs
@@ -1,19 +1,31 @@
-//! A trait for accessing I/O ports.
+//! Traits for accessing I/O ports.
 
-/// A helper trait that implements the actual port operations.
+/// A helper trait that implements the read port operation.
 ///
 /// On x86, I/O ports operate on either `u8` (via `inb`/`outb`), `u16` (via `inw`/`outw`),
 /// or `u32` (via `inl`/`outl`). Therefore this trait is implemented for exactly these types.
-pub trait PortReadWrite {
+pub trait PortRead {
     /// Reads a `Self` value from the given port.
     ///
     /// This function is unsafe because the I/O port could have side effects that violate memory
     /// safety.
     unsafe fn read_from_port(port: u16) -> Self;
+}
 
+/// A helper trait that implements the write port operation.
+///
+/// On x86, I/O ports operate on either `u8` (via `inb`/`outb`), `u16` (via `inw`/`outw`),
+/// or `u32` (via `inl`/`outl`). Therefore this trait is implemented for exactly these types.
+pub trait PortWrite {
     /// Writes a `Self` value to the given port.
     ///
     /// This function is unsafe because the I/O port could have side effects that violate memory
     /// safety.
     unsafe fn write_to_port(port: u16, value: Self);
 }
+
+/// A helper trait that implements the read/write port operations.
+///
+/// On x86, I/O ports operate on either `u8` (via `inb`/`outb`), `u16` (via `inw`/`outw`),
+/// or `u32` (via `inl`/`outl`). Therefore this trait is implemented for exactly these types.
+pub trait PortReadWrite: PortRead + PortWrite {}    
