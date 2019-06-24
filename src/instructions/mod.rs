c4e173049cb8fca101e2e75b483861b71d3b710c
Matt Taylor <mstaveleytaylor@gmail.com>
2019-06-24 22:21:11 +0100

    Add bochs magic breakpoint, read instruction pointer, inline individual instructions and various doc changes

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -10,9 +10,34 @@ pub mod tables;
 pub mod tlb;
 
 /// Halts the CPU until the next interrupt arrives.
-#[inline(always)]
+#[inline]
 pub fn hlt() {
     unsafe {
         asm!("hlt" :::: "volatile");
     }
+}
+
+/// Emits a 'magic breakpoint' instruction for the [Bochs](http://bochs.sourceforge.net/) CPU
+/// emulator. Make sure to set `magic_break: enabled=1` in your `.bochsrc` file.
+#[inline]
+pub fn bochs_breakpoint() {
+    unsafe {
+        asm!("xchgw %bx, %bx" :::: "volatile");
+    }
+}
+
+/// Gets the current instruction pointer. Note that this is only approximate as it requires a few
+/// instructions to execute.
+#[inline]
+pub fn read_rip() -> u64 {
+    let rip: u64;
+    unsafe {
+        asm!(
+            "call .next
+            .next:
+            pop $0"
+            : "=r"(rip) ::: "volatile"
+        );
+    }
+    rip
 }
--- src/instructions/tables.rs
+++ src/instructions/tables.rs
@@ -4,17 +4,26 @@ use crate::structures::gdt::SegmentSelector;
 
 pub use crate::structures::DescriptorTablePointer;
 
-/// Load GDT table.
+/// Load a GDT. Use the
+/// [`GlobalDescriptorTable`](crate::structures::gdt::GlobalDescriptorTable) struct for a high-level
+/// interface to loading a GDT.
+#[inline]
 pub unsafe fn lgdt(gdt: &DescriptorTablePointer) {
     asm!("lgdt ($0)" :: "r" (gdt) : "memory");
 }
 
-/// Load IDT table.
+/// Load an IDT. Use the
+/// [`InterruptDescriptorTable`](crate::structures::idt::InterruptDescriptorTable) struct for a high-level
+/// interface to loading an IDT.
+#[inline]
 pub unsafe fn lidt(idt: &DescriptorTablePointer) {
     asm!("lidt ($0)" :: "r" (idt) : "memory");
 }
 
-/// Load the task state register using the `ltr` instruction.
+/// Load the task state register using the `ltr` instruction. Use the
+/// [`GlobalDescriptorTable`](crate::structures::gdt::GlobalDescriptorTable) struct for a
+/// high-level interface to loading a TSS.
+#[inline]
 pub unsafe fn load_tss(sel: SegmentSelector) {
     asm!("ltr $0" :: "r" (sel.0));
 }
