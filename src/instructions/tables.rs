66b2e9aadeac0e1c215970a068530b96fca2af35
Matt Taylor <mstaveleytaylor@gmail.com>
2019-06-24 22:43:06 +0100

    Fix wording on load_tss function

--- src/instructions/tables.rs
+++ src/instructions/tables.rs
@@ -20,9 +20,7 @@ pub unsafe fn lidt(idt: &DescriptorTablePointer) {
     asm!("lidt ($0)" :: "r" (idt) : "memory");
 }
 
-/// Load the task state register using the `ltr` instruction. Use the
-/// [`GlobalDescriptorTable`](crate::structures::gdt::GlobalDescriptorTable) struct for a
-/// high-level interface to loading a TSS.
+/// Load the task state register using the `ltr` instruction.
 #[inline]
 pub unsafe fn load_tss(sel: SegmentSelector) {
     asm!("ltr $0" :: "r" (sel.0));
