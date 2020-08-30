0b2bf80eeb22f9e8452b604624704e52d895b6d0
Philipp Oppermann <dev@phil-opp.com>
2020-08-30 14:42:11 +0200

    Add a function for the `nop` instruction

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -23,6 +23,25 @@ pub fn hlt() {
     }
 }
 
+/// Executes the `nop` instructions, which performs no operation (i.e. does nothing).
+///
+/// This operation is useful to work around the LLVM bug that endless loops are illegally
+/// optimized away (see https://github.com/rust-lang/rust/issues/28728). By invoking this
+/// instruction (which is marked as volatile), the compiler should no longer optimize the
+/// endless loop away.
+#[inline]
+pub fn nop() {
+    #[cfg(feature = "inline_asm")]
+    unsafe {
+        llvm_asm!("nop" :::: "volatile");
+    }
+
+    #[cfg(not(feature = "inline_asm"))]
+    unsafe {
+        crate::asm::x86_64_asm_nop();
+    }
+}
+
 /// Emits a '[magic breakpoint](https://wiki.osdev.org/Bochs#Magic_Breakpoint)' instruction for the [Bochs](http://bochs.sourceforge.net/) CPU
 /// emulator. Make sure to set `magic_break: enabled=1` in your `.bochsrc` file.
 #[cfg(feature = "inline_asm")]
