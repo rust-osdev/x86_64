4c17ce90f7bd3bd5160b6dcc02513f9028d21338
toku_san <tokusan441@gmail.com>
2020-09-24 16:21:06 +0900

    Fix build error on latest nightly (#182)
    See: https://github.com/rust-lang/rust/issues/77134

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(feature = "const_fn", feature(const_fn))]
+#![cfg_attr(feature = "const_fn", feature(const_mut_refs))]
 #![cfg_attr(feature = "const_fn", feature(const_in_array_repeat_expressions))]
 #![cfg_attr(feature = "inline_asm", feature(llvm_asm))]
 #![cfg_attr(feature = "abi_x86_interrupt", feature(abi_x86_interrupt))]
