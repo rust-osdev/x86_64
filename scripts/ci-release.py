9cbf7eb7443b0f11da9ebab6f1b81c203e442094
Philipp Oppermann <dev@phil-opp.com>
2021-07-19 08:12:52 +0200

    Create a basic release script

---
+++ scripts/ci-release.py
@@ -0,0 +1,31 @@
+import toml
+import requests
+import subprocess
+
+cargo_toml = toml.load("Cargo.toml")
+crate_version = cargo_toml["package"]["version"]
+print("Detected crate version " + crate_version)
+
+api_url = "https://crates.io/api/v1/crates/x86_64/versions"
+crates_io_versions = requests.get(api_url).json()
+
+new_version = True
+for version in crates_io_versions["versions"]:
+    assert (version["crate"] == "x86_64")
+    if version["num"] == crate_version:
+        new_version = False
+        break
+
+if new_version:
+    print("Could not find version " + crate_version + " on crates.io; creating a new release")
+
+    tag_name = "v" + crate_version
+    print("  Tagging commit as " + tag_name)
+    subprocess.run(["git", "tag", tag_name], check=True)
+
+    print("  Running `cargo publish`")
+    subprocess.run(["cargo", "publish", "--dry-run"], check=True)
+
+    print("  Done")
+else:
+    print("Version " + crate_version + " already exists on crates.io")
