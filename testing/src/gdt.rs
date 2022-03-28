bce9ff530bf43c82730f3efe2c61d93a3a07f6fa
Joe Richey <joerichey@google.com>
2022-03-28 11:21:41 -0700

    Update bootloader tests

--- testing/src/gdt.rs
+++ testing/src/gdt.rs
@@ -13,7 +13,7 @@ lazy_static! {
             static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
 
             let stack_start = VirtAddr::from_ptr(unsafe { &STACK });
-            let stack_end = stack_start + STACK_SIZE;
+            let stack_end = stack_start + STACK_SIZE as u64;
             stack_end
         };
         tss
