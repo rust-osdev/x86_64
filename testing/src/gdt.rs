bb79df38483315853cef0f223a16d1bb567a7cbe
Joe Richey <joerichey@google.com>
2022-04-21 02:07:14 -0700

    Use staic TSS in testing
    Signed-off-by: Joe Richey <joerichey@google.com>

--- testing/src/gdt.rs
+++ testing/src/gdt.rs
@@ -1,23 +1,21 @@
 use lazy_static::lazy_static;
 use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable, SegmentSelector};
 use x86_64::structures::tss::TaskStateSegment;
-use x86_64::VirtAddr;
+use x86_64::VirtPtr;
 
 pub const DOUBLE_FAULT_IST_INDEX: u8 = 0;
 
-lazy_static! {
-    static ref TSS: TaskStateSegment = {
-        let mut tss = TaskStateSegment::new();
-        tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
-            const STACK_SIZE: usize = 4096;
-            static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
+const STACK_SIZE: usize = 4096;
+static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
 
-            let stack_start = VirtAddr::from_ptr(unsafe { &STACK });
-            let stack_end = stack_start + STACK_SIZE as u64;
-            stack_end
-        };
-        tss
-    };
+static TSS: TaskStateSegment = {
+    let mut tss = TaskStateSegment::new();
+    let stack_end = VirtPtr::from_slice(unsafe { &STACK }).end;
+    tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = stack_end;
+    tss
+};
+
+lazy_static! {
     static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
         // Add an unused segment so we get a different value for CS
